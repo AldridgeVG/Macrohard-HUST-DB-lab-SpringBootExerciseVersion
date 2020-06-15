f96ea51d434ecae63e97e37fa6fff52abe44a45e
aldridgevg <caxuser@live.com>
2020-06-16 05:09:03 +0800

    added query overwork record

--- src/main/java/com/Macrohard/Controller/absrowviLabController.java
+++ src/main/java/com/Macrohard/Controller/absrowviLabController.java
@@ -2,6 +2,7 @@ package com.Macrohard.Controller;
 
 import com.Macrohard.dao.AbsrOvwiLabDao;
 import com.Macrohard.entities.AbsRecord;
+import com.Macrohard.entities.OvwRecord;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,5 +19,14 @@ public class absrowviLabController {
         model.addAttribute("Absrs",mAbsrs);
 
         return "recordlab/queryAbsr_listAll";
+    }
+
+    @GetMapping("/ovwis")
+    public String ovwIsIndex(Model model){
+
+        List<OvwRecord> mOvwis = new AbsrOvwiLabDao().getAllOvwRecords();
+        model.addAttribute("Ovwis",mOvwis);
+
+        return "recordlab/queryOvwi_listAll";
     }
 }
--- src/main/java/com/Macrohard/dao/AbsrOvwiLabDao.java
+++ src/main/java/com/Macrohard/dao/AbsrOvwiLabDao.java
@@ -1,6 +1,7 @@
 package com.Macrohard.dao;
 
 import com.Macrohard.entities.AbsRecord;
+import com.Macrohard.entities.OvwRecord;
 import com.Macrohard.entities.empIDNameSet;
 import org.springframework.stereotype.Repository;
 
@@ -61,4 +62,53 @@ public class AbsrOvwiLabDao {
 
     }
 
+    public List<OvwRecord> getAllOvwRecords(){
+        List<OvwRecord> ret = new ArrayList<>();
+
+        //here use root to get users, after this we use our username
+        String url = "Jdbc:mysql://localhost:3306/macrohard?useSSL=false&serverTimezone=UTC";
+        String usr = "root";
+        String pwd = "galland990531";
+
+        Statement stmt = null;
+        Connection connection = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // load specific driver class
+            connection = DriverManager.getConnection(url, usr, pwd);
+
+            stmt = connection.createStatement();
+            String sql = "select * from overworkinfo"; // query into sheet1
+            rs = stmt.executeQuery(sql); // return result set
+
+            while (rs.next()) {
+                Integer empno1 = rs.getInt("employerno");
+                Integer mthno1 = rs.getInt("mthno");
+                String owtype1 = rs.getString("owtype");
+                Integer owhr1 = rs.getInt("owhr");
+                OvwRecord tmp = new OvwRecord(empno1,mthno1,owtype1,owhr1);
+                ret.add(tmp);
+            }
+            return ret;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (rs != null)
+                    rs.close();
+                if (stmt != null)
+                    stmt.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
